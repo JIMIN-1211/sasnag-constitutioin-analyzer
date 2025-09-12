ff95c146c1e2f3de34e17004171c52be13ec114f
ghghghzzp1 <ghghghzzp1@gmail.com>
2025-09-13 02:08:10 +0900

    Delete src directory

--- src/lib/password.js
+++
@@ -1,4 +0,0 @@
-const bcrypt = require('bcrypt');
-const hash = (plain) => bcrypt.hash(plain, 12);
-const verify = (plain, hashed) => bcrypt.compare(plain, hashed);
-module.exports = { hash, verify };
--- src/server.js
+++
@@ -1,3 +0,0 @@
-const { app } = require('./app');
-const PORT = Number(process.env.PORT || 3000);
-app.listen(PORT, () => console.log(`API listening http://localhost:${PORT}`));
